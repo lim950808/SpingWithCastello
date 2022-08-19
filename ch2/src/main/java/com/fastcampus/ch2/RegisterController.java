bcb1e797947b526e79e430db8cc065398ee942ef
lim950808 <lim080895@gmail.com>
2022-08-20 00:15:50 +0900

    Redirect, Forward

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller // ctrl+shift+o 자동 임포트 
 public class RegisterController {
-//	@RequestMapping(value="/register/add", method=RequestMethod.GET) // 신규회원 가입
+	@RequestMapping(value="/register/add", method= {RequestMethod.GET, RequestMethod.POST}) // 신규회원 가입
 //	@GetMapping("/register/add") // 4.3부터 추가
-//	public String register() {
-//		return "registerForm";  // WEB-INF/views/registerForm.jsp
-//	}
+	public String register() {
+		return "registerForm";  // WEB-INF/views/registerForm.jsp
+	}
 	
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST) // 신규회원 가입
-// 	@PostMapping("/register/save")
 	@PostMapping("/register/save")
 	public String save(User user, Model m) throws Exception {
 	//public String save(@ModelAttribute("user") User user, Model m) throws Exception {
@@ -28,7 +28,7 @@ public class RegisterController {
 			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
 			
 			m.addAttribute("msg", msg);
-			return "redirect:/register/add"; // 신규회원 가입화면으로 이동(redirect)
+			return "forward:/register/add"; // 신규회원 가입화면으로 이동(redirect)
 			//return "redirect:/register/add?msg="+msg; //URL재작성(rewriting)
 		}
 		
@@ -36,7 +36,7 @@ public class RegisterController {
 	}
 
 	private boolean isValid(User user) {
-		return true;
+		return false;
 	}
 }
 
